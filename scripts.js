d28474dc2d370d3aec164e0799985b0251fe48c1
Renee <156127959+reneexie01@users.noreply.github.com>
2024-07-05 06:55:46 +1000

    Added the html input and linked it to the playGame function

--- scripts.js
+++ scripts.js
@@ -28,44 +28,75 @@ class Player {
 }
 
 function playGame() {
-    const player1 = new Player('Warrior');
-    const player2 = new Player('Wizard');
+
+    // TO DO: link the values from the html inputs to the class constructed players upon submission
+
+    let player1, player2;
     let gameRound = 0;
+    let gameActive = false;
+
+    const startGame = function() {
+
+        const player1Input = document.querySelector("#player1").value;
+        const player2Input = document.querySelector("#player2").value;
+
+        const startButton = document.querySelector(".start");
+
+        startButton.addEventListener("click", function() {
+            if (player1Input === "" || player2Input === ""){
+                return;
+            } else {
+                player1 = new Player(player1Input);
+                player2 = new Player(player2Input);   
+                gameActive = true;       
+                console.log(player1);
+                console.log(player2); 
+                console.log(gameActive);
+            }
+        })
+    }
 
     const getPlayer1 = () => player1;
     const getPlayer2 = () => player2;
 
     const checkAlive = function(user) {
+
         if (!user.isAlive) {
             return `${user.name} has died.`;
         }
         return `${user.name} is still alive with ${user.hp} HP.`;
+
     }
 
     const play = function(attack) {
-        let attacker, opponent;
 
-        if (gameRound === 0 || gameRound % 2 === 0) {
-            attacker = getPlayer1();
-            opponent = getPlayer2();
-        } else {
-            attacker = getPlayer2();
-            opponent = getPlayer1();
-        }
+        if (gameActive) {
+            let attacker, opponent;
 
-        if (attacker.isAlive && opponent.isAlive) {
-            const attackResult = attacker[attack](opponent);
-            console.log(attackResult);
-            console.log(checkAlive(attacker));
-            console.log(checkAlive(opponent));
-            gameRound++;
-        } else {
-            return `Someone has died.`;
+            if (gameRound === 0 || gameRound % 2 === 0) {
+                attacker = getPlayer1();
+                opponent = getPlayer2();
+            } else {
+                attacker = getPlayer2();
+                opponent = getPlayer1();
+            }
+    
+            if (attacker.isAlive && opponent.isAlive) {
+                const attackResult = attacker[attack](opponent);
+                console.log(attackResult);
+                console.log(checkAlive(attacker));
+                console.log(checkAlive(opponent));
+                gameRound++;
+            } else {
+                return `Someone has died.`;
+            }
         }
 
     }
 
-    return { play, getPlayer1, getPlayer2 };
+    return { play, startGame };
 }
 
 const game = playGame();
+
+game.startGame();
